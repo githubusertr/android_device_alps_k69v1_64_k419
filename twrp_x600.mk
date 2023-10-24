02b927e0f26dfc791b65e41976d73f1685a6afdc
githubusertr <136189931+githubusertr@users.noreply.github.com>
2023-10-24 21:04:34 +0300

    Update and rename omni_k69v1_64_k419.mk to twrp_x600.mk

--- twrp_x600.mk
+++ twrp_x600.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from x600 device
 $(call inherit-product, device/omix/x600/device.mk)
 
 PRODUCT_DEVICE := x600
-PRODUCT_NAME := omni_x600
+PRODUCT_NAME := twrp_x600
 PRODUCT_BRAND := omix
 PRODUCT_MODEL := x600
 PRODUCT_MANUFACTURER := omix
