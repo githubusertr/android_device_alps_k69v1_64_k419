f7e9c7480df5f72ea23cb9adf94d16e357556664
githubusertr <136189931+githubusertr@users.noreply.github.com>
2023-10-24 20:49:19 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,5 +4,12 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+LOCAL_PATH := device/omix/x600
 
-LOCAL_PATH := device/alps/k69v1_64_k419
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
